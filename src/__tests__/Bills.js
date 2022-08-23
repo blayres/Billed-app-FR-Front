29231a20c288e74c93c6d1e6478249c7ef530c53
blayres <barbara.ayres.pro@gmail.com>
2022-08-23 12:55:51 +0200

    newbill ok

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -126,18 +126,6 @@ describe("Given I am connected as an employee", () => {
 describe("Given I am a user connected as Employee", () => {
   describe("When I navigate to Bills Page", () => { 
     test("fetches bills from mock API GET", async () => {
-      // localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
-      // const root = document.createElement("div")
-      // root.setAttribute("id", "root")
-      // document.body.append(root)
-      // router()
-      // window.onNavigate(ROUTES_PATH.Bills)
-      // await waitFor(() => screen.getByText("Validations"))
-      // const contentPending  = await screen.getByText("En attente (1)")
-      // expect(contentPending).toBeTruthy()
-      // const contentRefused  = await screen.getByText("Refusé (2)")
-      // expect(contentRefused).toBeTruthy()
-      // expect(screen.getByTestId("big-billed-icon")).toBeTruthy()
     })
   describe("When an error occurs on API", () => {
     beforeEach(() => {
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,25 +2,33 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen, waitFor } from "@testing-library/dom"
-import userEvent from '@testing-library/user-event';
-import BillsUI from '../views/BillsUI.js';
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-import '@testing-library/jest-dom';
-import { localStorageMock } from '../__mocks__/localStorage';
-import Store from '../app/Store.js'
-import store from '../__mocks__/store';
-import { ROUTES, ROUTES_PATH } from '../constants/routes';
-import mockStore from "../__mocks__/store"
-import { bills, newBill } from "../fixtures/bills.js";
+ import { screen, fireEvent, waitFor } from "@testing-library/dom"
+ import NewBillUI from "../views/NewBillUI.js"
+ import NewBill from "../containers/NewBill.js"
+ import { localStorageMock } from "../__mocks__/localStorage.js"
+ import mockStore from "../__mocks__/store"
+ import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+ import userEvent from "@testing-library/user-event";
+ import router from "../app/Router";
 
-// jest.mock("../app/store", () => mockStore)
+ jest.mock("../app/Store", () => mockStore)
+
 
 // init onNavigate
 const onNavigate = (pathname) => {
   document.body.innerHTML = ROUTES({ pathname });
 };
+
+beforeEach(() => {
+  //On simule la connection sur la page Employee en parametrant le localStorage
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  window.localStorage.setItem('user', JSON.stringify({
+    type: 'Employee',
+    email: 'employee@test.tld'
+  }))
+  // Afficher la page nouvelle note de frais
+  document.body.innerHTML = NewBillUI()
+})
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -59,7 +67,7 @@ describe('When I am on NewBill Page and I add an attached file', () => {
     const newBill = new NewBill({
       document,
       onNavigate,
-      Store: Store,
+      Store: mockStore,
       localStorage: window.localStorage,
     });
 
@@ -148,73 +156,83 @@ describe('When I am on NewBill page and I submit a wrong attached file format', 
 });
 
 
+// TEST submit form
+describe("NewBill Integration Test Suites", () => {
+  describe("Given I am auser connected as an employee", () => {
+    describe("When I am on NewBill", () => {
+      test("Then I submit completed NewBill form and I am redirected on Bill, methode Post", async() => {
+      // route
+      document.body.innerHTML = `<div id="root"></div>`;
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      // value for Expense-name
+      const expenseName = screen.getByTestId("expense-name")
+      expenseName.value = 'vol'
+      // value for Datepicker
+      const datepicker = screen.getByTestId("datepicker")
+      datepicker.value = '2022-08-22'
+      // value for Amount
+      const amount = screen.getByTestId("amount")
+      amount.value = '300'
+      // value for Vat
+      const vat = screen.getByTestId("vat")
+      vat.value ='40'
+      // value for Pct
+      const pct = screen.getByTestId("pct")
+      pct.value ='50'
+      // File and fireEvent
+      const file = screen.getByTestId("file")
+      fireEvent.change(file, {
+        target: {
+          files: [new File(['image.png'], 'image.png', { type: 'image/png'})],
+        },
+      })
+      // Form Submission
+      const formSubmission = screen.getByTestId("form-new-bill")
+      const newBillEmulation = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+     
+      const handleSubmit = jest.fn((e) => newBillEmulation.handleSubmit(e))
+      // addEventListener on form
+      formSubmission.addEventListener('submit', handleSubmit)
+      fireEvent.submit(formSubmission)
+      expect(handleSubmit).toHaveBeenCalled()
+      await waitFor(() => screen.getAllByText("Mes notes de frais"))
+      expect(screen.getByTestId('btn-new-bill')).toBeTruthy()
+     })
+    })
+  })
+})
+
+
 
 
 
 // POST TESTS
 describe('Given I am connected as an employee', () => {
   describe('When I am on NewBill Page and submit the form', () => {
-    test('Then it should generate a new bill', async () => {
-      // spy
-      // Cannot spy the post property because it is not a function
-      // undefined given instead
-      const postSpy = jest.spyOn(mockStore, 'bills');
 
-      // new bill to submit
-      const newBill = {
-        id: '47qAXb6fIm2zOKkLzMro',
-        vat: '80',
-        fileUrl:
-          'https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a',
-        status: 'pending',
-        type: 'Hôtel et logement',
-        commentary: 'séminaire billed',
-        name: 'fake new bill',
-        fileName: 'preview-facture-free-201801-pdf-1.jpg',
-        date: '2004-04-04',
-        frenchDate: '04-04-2004',
-        amount: 400,
-        commentAdmin: 'ok',
-        email: 'a@a',
-        pct: 20,
-      };
+    test('create a new bill from mock API POST', async () => {
 
-      // get bills and the new bill
-      const bills = await mockStore.post(newBill);
+      const bill = [{
+        "id": "47qAXb6fIm2zOKkLzMro",
+        "vat": "80",
+        "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+        "status": "pending",
+        "type": "Hôtel et logement",
+        "commentary": "séminaire billed",
+        "name": "encore",
+        "fileName": "preview-facture-free-201801-pdf-1.jpg",
+        "date": "2004-04-04",
+        "amount": 400,
+        "commentAdmin": "ok",
+        "email": "a@a",
+        "pct": 20
+      }]
+      const callStore = jest.spyOn(mockStore, 'bills');
 
-      // expected results
-      expect(postSpy).toHaveBeenCalledTimes(1);
-      expect(bills).toBe('fake new bill received');
-    });
+      mockStore.bills().create(bill);
 
-    // TEST : newBill fetch failure => 404 error
-    test('Then the bill is added to the API but fails with 404 message error', async () => {
-      mockStore.bills.mockImplementationOnce(() => {
-        return {
-          list : () =>  {
-            return Promise.reject(new Error("Erreur 404"))
-          }
-        }})
-      window.onNavigate(ROUTES_PATH.NewBill)
-      await new Promise(process.nextTick);
-      const message = await screen.getByText(/Erreur 404/)
-      expect(message).toBeTruthy()
-    })
-
-    test('then it posts to API and fails with 500 message error on Bills page', async () => {
-      // cannot read property 'mockImplementationOnce' of undefined
-      mockStore.bills.mockImplementationOnce(() =>
-        Promise.reject(new Error('Erreur 500'))
-      );
-
-      // DOM construction
-      document.body.innerHTML = BillsUI({ error: 'Erreur 500' });
-
-      // await for response
-      const message = screen.getByText(/Erreur 500/);
-
-      // expected result
-      expect(message).toBeTruthy();
+      expect(callStore).toHaveBeenCalled();
     });
   });
 });
